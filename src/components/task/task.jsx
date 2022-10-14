63919cda48de2aeaa28f360efa41722a51d6464a
piotr.selak <p.selak.848@studms.ug.edu.pl>
2022-10-14 17:00:24 +0200

    better stylized

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -3,8 +3,10 @@ import "./task.css";
 export default function Task({ text, onClick }) {
   return (
     <div className="task">
-      {text}
-      <button onClick={onClick}>&#9747;</button>
+      <p>{text}</p>
+      <button class="delete-button" onClick={onClick}>
+        &#9747;
+      </button>
     </div>
   );
 }
