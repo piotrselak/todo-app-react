3278b48d38ddaa4210d322850fc7a08aefb28baa
piotr.selak <p.selak.848@studms.ug.edu.pl>
2022-10-14 12:15:47 +0200

    removed unnecessary button component

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "../css/todoList.css";
-import Button from "./button";
 
-export default function Task({ text, onClick, buttonType }) {
+export default function Task({ text, onClick }) {
   return (
     <div className="task">
       {text}
-      <Button type={buttonType} onClick={onClick}>
-        &#9747;
-      </Button>
+      <button onClick={onClick}>&#9747;</button>
     </div>
   );
 }
